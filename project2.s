e231e4eb348f22ca6234e4b93eb3d16faf78d5fe
KyraSwift <42423443+KyraSwift@users.noreply.github.com>
2018-11-16 19:09:21 -0500

    Data Declarations
    Begin with Data Declaration (.data) - 
    Variable EMPTYINPUT will print (.asciiz) "Input is Empty" if the string has zero characters
    Variable INVALIDINPUT will print (.asciiz) "Invalid base-33 number." if the string contains characters not included in list
    Variable LONGINPUT will print (.asciiz) "Input is too long." if string is more than 4 characters 
    Variable USERINPUT will take a user input - (.space len (512)) allocates 512 consecutive bytes

---
+++ project2.s
@@ -0,0 +1,8 @@
+#Kyra Swift @02852494
+#Project2.s
+.data
+  EmptyInput:   .asciiz "Input is empty." #String has Zero Characters - Print Input is Empty
+  InvalidInput: .asciiz "Invalid base-33 number." #String includes character not in list - Print invalid base-33 number
+  LongInput:    .asciiz "Input is too long." #String has more than 4 characters - Print Input is too long
+  UserInput:    .space  512
+  
